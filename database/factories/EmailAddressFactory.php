1cbc76d19cf2de21297d52b933ae20f0cd015774
Teranveril <adam.holow@gmail.com>
2025-05-17 00:35:56 +0200

    nnjkljjjkhnj

---
+++ database/factories/EmailAddressFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\EmailAddress;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EmailAddressFactory extends Factory
+{
+    protected $model = EmailAddress::class;
+
+    public function definition(): array
+    {
+        return [
+            'email' => $this->faker->unique()->safeEmail(),
+            // user_id ustawi test: ->create(['user_id' => …])
+        ];
+    }
+}
