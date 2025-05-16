6e23fc10cee18b10a8c8c6e17e75e39a97b3e3a0
Teranveril <adam.holow@gmail.com>
2025-05-16 23:53:24 +0200

    nn

--- database/factories/EmailAdressFactory.php
+++ database/factories/EmailAdressFactory.php
@@ -6,9 +6,6 @@ use App\Models\EmailAddress;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
- */
 class EmailAdressFactory extends Factory
 {
     /**
